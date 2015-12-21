15f408e9d9b90c5c3580edc55ce54da284b71fe4
Jacob Wood <xjacobwood@gmail.com>
2015-12-21 02:09:23 -0500

    Added routes we will need

--- server/app.py
+++ server/app.py
@@ -6,4 +6,17 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello, EasyCTF!"
 
+@app.route('/login')
+def login():
+	return
+@app.route('/register')
+def register():
+	return
+@app.route('/scoreboard')
+def scoreboard():
+	return
+@app.route('/problems')
+def problems():
+	return
+
 app.run(port=8000)
