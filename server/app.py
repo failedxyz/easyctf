159b0401eff594ba9feb1cc8f33251292ec70dd9
Thomas Gerot <aerodynamicalpa@hotmail.com>
2015-12-21 12:10:36 -0600

    Filled return statements with placeholder messages

--- server/app.py
+++ server/app.py
@@ -1,25 +1,29 @@
 from flask import Flask
-
 import sys
 
 app = Flask(__name__)
 
+#Home Page
 @app.route("/")
 def hello_world():
 	return "Hello, EasyCTF!"
-
+#Login Page
 @app.route('/login')
 def login():
-	return
+	return "EasyCTF Login"
+#Registration Page
 @app.route('/register')
 def register():
-	return
+	return "EasyCTF Register"
+#Scoreboard Page
 @app.route('/scoreboard')
 def scoreboard():
-	return
+	return "EasyCTF Scoreboard"
+#Problems Page
 @app.route('/problems')
 def problems():
-	return
+	return "EasyCTF Problems"
+
 if __name__ == "__main__":
     app.debug = "--debug" in sys.argv
     app.run(port=8000)
