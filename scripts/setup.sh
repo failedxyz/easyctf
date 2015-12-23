44cc24b1af5e7657270cddca3e6f73739023e049
James Wang <jameswang9909@hotmail.com>
2015-12-22 20:17:39 -0500

    Add mysql dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 echo "Installing dependencies..."
-sudo apt-get -y install python-pip
+sudo apt-get -y install python-pip libmysqlclient-dev
 
 echo "Installing pip dependencies..."
 sudo pip install -r scripts/requirements.txt
