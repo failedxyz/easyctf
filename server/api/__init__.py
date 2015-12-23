de0ba444e6e535f0e97fb6ee987171175bfef4f0
Michael Zhang <failed.down@gmail.com>
2015-12-23 17:06:44 -0600

    added user model

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,1 +1,3 @@
 import models
+import utils
+import user
--- server/api/models.py
+++ server/api/models.py
@@ -1,4 +1,20 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+import api.utils
 
 db = SQLAlchemy()
 
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64))
+	username = db.Column(db.String(64), unique=True)
+	username_lower = db.Column(db.String(64), unique=True)
+	email = db.Column(db.String(64), unique=True)
+	password = db.Column(db.String(128))
+	admin = db.Column(db.Boolean)
+
+	def __init__(self, name, username, email, password):
+		self.name = name
+		self.username = username
+		self.username_lower = username.lower()
+		self.email = email.lower()
+		self.password = api.utils.hash_password(password)
