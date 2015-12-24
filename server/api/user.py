dc262f778ae8dd06c308e3b1bedfdb12b38128fb
James Wang <jameswang9909@hotmail.com>
2015-12-23 21:06:49 -0500

    Begin work on user registration

--- server/api/user.py
+++ server/api/user.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, session
+from flask import Blueprint, session, request
+from flask import current_app as app
+
+from models import db, Users
 from utils import api_wrapper
 
 blueprint = Blueprint("user", __name__)
@@ -6,7 +9,31 @@ blueprint = Blueprint("user", __name__)
 @blueprint.route("/register", methods=["POST"])
 @api_wrapper
 def user_register():
-	return { "success": 0, "message": "Registration is not open yet." }
+    name = request.form["name"]
+    username = request.form["username"]
+    password = request.form["password"]
+    password_confirm = request.form["password_confirm"]
+    email = request.form["email"]
+
+    username_exists = Users.query.add_columns("name", "uid").filter_by(username=username).first()
+    email_exists = Users.query.add_columns("name", "uid").filter_by(email=email).first()
+
+    if password != password_confirm:
+        return { "success": 0, "message": "Passwords do not match." }
+    if len(password) > 128:
+        return { "success": 0, "message": "Password is too long." }
+    if len(password) == 0:
+        return { "success": 0, "message": "Password is too short." }
+    if len(username) > 64:
+        return { "success": 0, "message": "Username is too long." }
+    if username_exists:
+        return { "success": 0, "message": "Username is already taken." }
+    if email_exists:
+        return { "success": 0, "message": "Email has already been used." }
+
+    add_user(name, username, email, password)
+
+    return { "success": 1, "message": "Success!" }
 
 @blueprint.route("/logout", methods=["POST"])
 @api_wrapper
@@ -18,3 +45,8 @@ def user_logout():
 @api_wrapper
 def user_login():
     pass
+
+def add_user(name, username, email, password):
+    user = Users(name, username, email, password)
+    db.session.add(user)
+    db.session.commit()
--- server/config.py
+++ server/config.py
@@ -10,5 +10,9 @@ with open(".secret_key", "a+") as secret:
         key = secret.read()
 
 SECRET = key
+
+SQLALCHEMY_DATABASE_URI = "mysql://root:i_hate_passwords@localhost/easyctf"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 CTF_BEGIN = 0 # To be used later
 CTF_END = 0 # To be used later
