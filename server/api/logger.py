19a475199aa6d0b7b8e1cfd1b912451a792767d1
Michael Zhang <failed.down@gmail.com>
2016-01-12 22:41:59 -0600

    add more logs

--- server/api/logger.py
+++ server/api/logger.py
@@ -2,6 +2,9 @@ import datetime
 import logging
 import logging.handlers
 import os
+import pkgutil
+
+import api
 
 NOTSET = 0
 DEBUG = 10
@@ -11,33 +14,21 @@ ERROR = 40
 CRITICAL = 50
 
 def initialize_logs():
-    registration_logger = logging.getLogger("registrations")
-    login_logger = logging.getLogger("logins")
-    submission_logger = logging.getLogger("submissions")
+	def create_logger(name):
+		new_logger = logging.getLogger(name)
+		new_logger.setLevel(logging.INFO)
 
-    registration_logger.setLevel(logging.INFO)
-    login_logger.setLevel(logging.INFO)
-    submission_logger.setLevel(logging.INFO)
+		base = os.path.dirname(__file__).strip("api")
+		log_path = os.path.join(base, "logs")
+		if not os.path.exists(log_path):
+			os.mkdir(log_path)
 
-    base = os.path.dirname(__file__).strip("api")
-    log_path = os.path.join(base, "logs")
+		log_handler = logging.handlers.RotatingFileHandler(os.path.join(log_path, name + ".log"))
+		new_logger.addHandler(log_handler)
+	for importer, modname, ispkg in pkgutil.walk_packages(path="../api"):
+		create_logger(modname)
 
-    if not os.path.exists(log_path):
-        os.mkdir(log_path)
-
-    # logs = [os.path.join(log_path, "registrations.log"), os.path.join(log_path, "logins.log"), os.path.join(log_path, "submissions.log")]
-    logs = map(lambda x: os.path.join(log_path, x + ".log"), \
-        [ "registrations", "logins", "submissions", "create_team" ])
-
-    registration_log = logging.handlers.RotatingFileHandler(logs[0], maxBytes=10000)
-    login_log = logging.handlers.RotatingFileHandler(logs[1], maxBytes=10000)
-    submission_log = logging.handlers.RotatingFileHandler(logs[2], maxBytes=10000)
-
-    registration_logger.addHandler(registration_log)
-    login_logger.addHandler(login_log)
-    submission_logger.addHandler(submission_log)
-
-def log(log, level, message):
-    logger = logging.getLogger(log)
-    message = "[%s] %s" % (datetime.datetime.now().strftime("%m/%d/%Y %X"), message)
-    logger.log(level, message)
+def log(logname, level, message):
+	logger = logging.getLogger(logname)
+	message = "[%s] %s" % (datetime.datetime.now().strftime("%m/%d/%Y %X"), message)
+	logger.log(level, message)
