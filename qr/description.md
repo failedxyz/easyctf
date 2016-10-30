15c0e51f625d6435dad1e716984fbbdd4230d3b0
Crystal Su <crystal.buliao@gmail.com>
2016-10-30 11:54:20 -0700

    added finn and qr

---
+++ qr/description.md
@@ -0,0 +1,1 @@
+You know that cliché CTF problem where your QR code is missing a few pixels? Well guess what, we have a different [QR](${qr.png}) problem this year :D
---
+++ qr/grader.py
@@ -0,0 +1,4 @@
+def grade(key):
+    if key.find("that_wasn't_so_hard_n0w_was_it?") != -1:
+        return True, "Yay! Now please tell me you didn't actually paint it . . ."
+    return False, "Nope!"
