93577ddee37a489cf0aa1a4b987d23a3bc3d2657
Jacob Magnuson <blockingthesky@gmail.com>
2017-03-10 23:21:07 +0000

    removed dijkstra

--- dijkstra/grader.py
+++
@@ -1,4 +0,0 @@
-def grade(autogen, answer):
-    if answer.find("edsger_wybe_dijkstra_was_a_happy_accident") != -1:
-        return True, "Great Job! That's a tough one."
-    return False, "Nope, try again."
