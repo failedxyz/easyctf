f9a58afef003d40f3ada9c1645eda26363521cf3
Jacob Magnuson <blockingthesky@gmail.com>
2017-03-10 23:49:21 +0000

    rsa changes

--- rsa1/grader.py
+++ rsa1/grader.py
@@ -1,4 +1,4 @@
 def grade(autogen, key):
-    if key.find("tw0_v3ry_merrry_tw1n_pr1m35!!_417c0d") != -1:
-        return True, "Really Superb! Applause!"
-    return False, "RIP"
+    if key.find("todo") != -1:
+        return True, "Awesome job! Get ready for some harder ones :)"
+    return False, "Keep trying!"
--- rsa3/description.md
+++
@@ -1,1 +0,0 @@
-We came across another [message]($rsa3) that follows the same cryptographic schema as those other Really Scary Admin messages. Take a look and see if you can crack it.
