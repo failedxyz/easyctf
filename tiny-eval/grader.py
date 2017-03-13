5778ab4a5ac52364c224059a1331f7ad347481d0
Michael Zhang <failed.down@gmail.com>
2017-03-13 02:04:06 -0500

    Update tinyeval flag.

--- tiny-eval/grader.py
+++ tiny-eval/grader.py
@@ -1,4 +1,4 @@
 def grade(autogen, key):
-    if key.find("1t's_4lw4ys_a_G00d_idea_2_ch3ck_th3_he4d3rs!") != -1:
+    if key.find("it's_2017_who_still_uses_php?(jk_82.5%_of_websites)") != -1:
         return True, "You got it!"
     return False, "Nope. Keep poking around."
