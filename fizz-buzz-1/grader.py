73c78ce453f68374e1c952c3a2ec86b5c2c9687d
Brandon John <wiresboy@yahoo.com>
2017-03-13 18:09:28 -0500

    Implemented fizzbuzz graders, updated values

--- fizz-buzz-1/grader.py
+++ fizz-buzz-1/grader.py
@@ -1,12 +1,11 @@
-n = input()
+n = int(raw_input())
 
 for i in range(1, n + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        print 'FizzBuzz'
-    elif i % 3 == 0:
-        print 'Fizz'
-    elif i % 5 == 0:
-        print 'Buzz'
-    else:
-        print i
-        
+  if i % 3 == 0 and i % 5 == 0:
+    print 'FizzBuzz'
+  elif i % 3 == 0:
+    print 'Fizz'
+  elif i % 5 == 0:
+    print 'Buzz'
+  else:
+    print i
--- fizz-buzz-2/grader.py
+++ fizz-buzz-2/grader.py
@@ -1,4 +1,11 @@
-def grade(random, key):
-    if key.find("PUT A NEW KEY HERE!!!!") != -1:
-        return True, "Correct!"
-    return False, "Nope."
+n = int(raw_input())
+
+for i in range(1, n + 1):
+  if i % 3 == 0 and i % 5 == 0:
+    print 'FizzBuzz'
+  elif i % 3 == 0:
+    print 'Fizz'
+  elif i % 5 == 0:
+    print 'Buzz'
+  else:
+    print i
