14bc6b67d6b4675a93b43c3afd85dae3431f8def
Ankur Sundara <me@ankursundara.com>
2017-03-13 21:26:12 -0500

    Update grader.py

--- cookieblog/grader.py
+++ cookieblog/grader.py
@@ -1,4 +1,4 @@
 def grade(random, key):
         if key.find("yum_c00kies!!!") != -1:
                 return True, "Yum!"
-        return False, "Eww..."
+        return False, "Eww... wrong. bad cookie"
