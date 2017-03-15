970d61159d683fee6ba5f36239dd5f2a6a81175b
David Hou <david.hou314@gmail.com>
2017-03-15 09:32:08 -0700

    Fix source verifer for fizz buzz 2

--- fizz-buzz-2/source_verifier.py
+++ fizz-buzz-2/source_verifier.py
@@ -1,5 +1,8 @@
-code = raw_input()
-if ("i" in code) or ("I" in code) or ("?" in code):
-  print "Your program contains an 'i', 'I', or '?'. Please remove it and try again."
-else
+import sys                                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                                                                                                
+code = sys.stdin.read()                                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                
+if ("i" in code) or ("I" in code) or ("?" in code):                                                                                                                                                                                                                             
+  print "Your program contains an 'i', 'I', or '?'. Please remove it and try again."                                                                                                                                                                                            
+else:                                                                                                                                                                                                                                                                           
   print "OK"
