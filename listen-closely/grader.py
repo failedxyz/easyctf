b9382cb34f762363b52255ba7740ad56e73f0608
Michael Barre <mbarre@artandcraftentertainment.com>
2017-03-16 23:46:33 -0500

    new flag for listen closely

--- listen-closely/grader.py
+++ listen-closely/grader.py
@@ -1,4 +1,4 @@
 def grade(autogen, key):
-    if key.upper().find("CAN_YOU_EVEN_HEAR_ME_YET") != -1:
+    if key.upper().find("DO_YOU_HEAR_THE_C_SHELLS_BASHING_AGAINST_THE_C_SHORE") != -1:
         return True, "Correct!"
     return False, "Nope!"
